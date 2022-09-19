0595c3c3e1f3ba52ec26a7de6dd1606893e5502b
adell <baka_test_shoukann@yahoo.co.jp>
2022-09-19 20:11:10 +0900

    マスターデータとしてScriptableObjectの作成

---
+++ Assets/Script/StageListScriptableObjec.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/StageListScriptableObject", order = 1)]
+public class StageListScriptableObjects : ScriptableObject
+{
+    public List<StageScriptableObjects> StageList;
+
+}
---
+++ Assets/Script/StageScriptableObjects.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class StageScriptableObjects 
+{
+    public int id;
+    public string name;
+    public int unlock_condition_type;
+    public int unlock_condition_value;
+    public string thumbnail;
+    public string stage_scene;
+}
