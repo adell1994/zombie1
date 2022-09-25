ea167dee8bb0245482248f61e66c7983d1b55f2d
adell <baka_test_shoukann@yahoo.co.jp>
2022-09-25 19:53:33 +0900

    SelectSceneのマスターデータを使ったスクリプトの作成及び実装

---
+++ Assets/Script/Select/EntryPoint/Select.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Select : MonoBehaviour
+{
+    public StageListScriptableObjects stageList;
+    public GameObject stagePrefab;
+    public RectTransform listTransform;
+    public const int CONTENT_WIDTH = 300;
+    public const int CONTENT_SPACE = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int count = 0;
+        foreach (StageScriptableObjects stage in stageList.StageList)
+        {
+            GameObject obj = Instantiate(stagePrefab,listTransform);
+            obj.GetComponent<RectTransform>().localPosition = new Vector3(count * (CONTENT_WIDTH + CONTENT_SPACE), 0);
+            GameObject label = obj.transform.Find("Label").gameObject;
+            TextMeshProUGUI tmp = label.GetComponent<TextMeshProUGUI>();
+            tmp.text = stage.name;
+            Sprite sp = Resources.Load<Sprite>(stage.thumbnail);
+            Image img = obj.GetComponent<Image>();
+            img.sprite = sp;
+            count++;
+        }
+        listTransform.localPosition = new Vector3((-count * (CONTENT_WIDTH + CONTENT_SPACE) + CONTENT_SPACE) / 2  + CONTENT_WIDTH / 2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
