ba2c614993523f175fd711667b40079b9f8eb00f
adell <baka_test_shoukann@yahoo.co.jp>
2022-10-08 22:33:02 +0900

    SelectSceneの別シーンに移動するScriptの実装と移動先の表示名位置を調整

--- Assets/Script/Select/EntryPoint/Select.cs
+++ Assets/Script/Select/EntryPoint/Select.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Select : MonoBehaviour
 {
@@ -11,6 +12,10 @@ public class Select : MonoBehaviour
     public RectTransform listTransform;
     public const int CONTENT_WIDTH = 300;
     public const int CONTENT_SPACE = 200;
+    public AudioClip selectSE;
+    private AudioSource audioSource;
+    public bool isButtonPush = false;
+    private int id;
 
     // Start is called before the first frame update
     void Start()
@@ -29,11 +34,37 @@ public class Select : MonoBehaviour
             count++;
         }
         listTransform.localPosition = new Vector3((-count * (CONTENT_WIDTH + CONTENT_SPACE) + CONTENT_SPACE) / 2  + CONTENT_WIDTH / 2, 0);
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void ChengeScene()
+    {
+        if(id == 1)
+        {
+            SceneManager.LoadScene("Port");
+        }
+        if (id == 2)
+        {
+            SceneManager.LoadScene("Village");
+        }
+        if (id == 3)
+        {
+            SceneManager.LoadScene("Stage3");
+        }
+    }
+    public void PushStartButton()
+    {
+        if (isButtonPush == true)
+        {
+            return;
+        }
+        isButtonPush = true;
+        audioSource.PlayOneShot(selectSE);
+        Invoke("ChengeScene", 2.5f);
     }
 }
