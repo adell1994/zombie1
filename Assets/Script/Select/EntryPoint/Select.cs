4bb1c8bdb83bf23a92bdee603a6d6d1d911f46df
adell <baka_test_shoukann@yahoo.co.jp>
2023-04-01 17:41:16 +0900

    SelectScineのボタンを選択してもステージ移行しなかったバグの修正完了

--- Assets/Script/Select/EntryPoint/Select.cs
+++ Assets/Script/Select/EntryPoint/Select.cs
@@ -10,16 +10,16 @@ public class Select : MonoBehaviour
     public StageListScriptableObjects stageList;
     public GameObject stagePrefab;
     public RectTransform listTransform;
+    public AudioSource audioSource;
     public const int CONTENT_WIDTH = 300;
     public const int CONTENT_SPACE = 200;
     public AudioClip selectSE;
-    private AudioSource audioSource;
     public bool isButtonPush = false;
-    private int id;
 
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = gameObject.GetComponent<AudioSource>();
         int count = 0;
         foreach (StageScriptableObjects stage in stageList.StageList)
         {
@@ -31,6 +31,9 @@ public class Select : MonoBehaviour
             Sprite sp = Resources.Load<Sprite>(stage.thumbnail);
             Image img = obj.GetComponent<Image>();
             img.sprite = sp;
+            int id = stage.id;
+            obj.GetComponent<Button>().onClick.AddListener(() => ChengeScene(id));
+
             count++;
         }
         listTransform.localPosition = new Vector3((-count * (CONTENT_WIDTH + CONTENT_SPACE) + CONTENT_SPACE) / 2  + CONTENT_WIDTH / 2, 0);
@@ -42,18 +45,36 @@ public class Select : MonoBehaviour
     {
         
     }
-    public void ChengeScene()
+    public void ChengeScene(int id)
     {
         if(id == 1)
         {
+            if (isButtonPush == true)
+            {
+                return;
+            }
+            isButtonPush = true;
+            audioSource.PlayOneShot(selectSE);
             SceneManager.LoadScene("Port");
         }
         if (id == 2)
         {
+            if (isButtonPush == true)
+            {
+                return;
+            }
+            isButtonPush = true;
+            audioSource.PlayOneShot(selectSE);
             SceneManager.LoadScene("Village");
         }
         if (id == 3)
         {
+            if (isButtonPush == true)
+            {
+                return;
+            }
+            isButtonPush = true;
+            audioSource.PlayOneShot(selectSE);
             SceneManager.LoadScene("Stage3");
         }
     }
